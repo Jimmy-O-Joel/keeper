1cb6207b67b40b49b81dcece5144107094e6ad29
Jimmy Joel <jimmyjoel177@gmail.com>
2023-01-03 13:45:22 +0300

    add class container-fluid to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
+    <div className='container-fluid'>
       <header><h1>tHOUGHTS</h1></header>
     </div>
   )
